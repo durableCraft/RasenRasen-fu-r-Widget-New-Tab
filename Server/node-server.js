2fab2ef6788ddf19978f950faafa6831618ef671
durableCraft <plegerluca@gmail.com>
2024-01-19 10:25:08 +0100

    First changes to Sockets

--- Server/node-server.js
+++ Server/node-server.js
@@ -2,34 +2,59 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const path = require('path');
-const rateLimit = require('express-rate-limit');
+/* const rateLimit = require('express-rate-limit'); */
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// Rate Limiting für API-Endpunkte (z.B. Socket.io)
+/* // Rate Limiting für API-Endpunkte (z.B. Socket.io)
 const apiLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 Minute
     max: 3800, // Maximale Anzahl von Anfragen pro Minute
     message: 'Zu viele Anfragen von dieser IP, bitte versuche es später erneut.'
 });
 
-app.use('/api/', apiLimiter);
+app.use('/api/', apiLimiter); */
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../test.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-io.on('connection', (socket) => {
-    console.log('Ein Client hat sich verbunden.');
+/* const rasenPartikelAnzahl = 300;
+let rasenPositionen = [{}];
 
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function initialRasenFlaeche(amount) {
+    for (let index = 0; index < amount; index++) {
+        const RandPosX = getRandomInteger(1, canvas.width);
+        const RandPosY = getRandomInteger(1, canvas.height);
+
+        rasenPositionen.push([RandPosX, RandPosY]);
+    }
+}
+
+initialRasenFlaeche(rasenPartikelAnzahl); */
+
+let serverInfo = [{}];
+const fps = 60;
+
+function gamlogic() {
+    
+}
+
+io.on('connection', (socket) => {
     // Beispiel: Sende eine Variable an den Client
-    socket.emit('nachrichtVomServer', 'Hallo, Client!');
+    setInterval(() => {
+        socket.emit('serverInfo', serverInfo);
+    }, 1000 / fps);
 
     // Beispiel: Empfange eine Variable vom Client
-    socket.on('nachrichtVomClient', (data) => {
-        console.log('Nachricht vom Client:', data);
+    socket.on('clientInfo', (data) => {
+        serverInfo.push(data);
     });
 });
 
